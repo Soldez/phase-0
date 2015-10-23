b0d76926bcf37b1565006ca1981c2712d774d3bd
Kirb Fernandez <kirbfernandez@Kirbs-MacBook-Pro.local>
2015-10-23 01:27:51 -0700

    completed 4.3

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,13 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def do_something(name, city, state)
+	puts name
+	puts city
+	puts state
+end
+
---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,26 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ return num_1 + num_2
+end
+add(10,2)
+
+def subtract(num_1, num_2)
+ return num_1 - num_2
+end
+subtract(10,2)
+
+def multiply(num_1, num_2)
+ return num_1 * num_2
+end
+multiply(10,2)
+
+def divide(num_1, num_2)
+ return num_1.to_f / num_2.to_f
+end
+divide(10,2)
